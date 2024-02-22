b6df76a194814c7c61896e2e860b0ae7945974e3
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-22 08:42:09 -0300

    ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+real="";
+dolar=4.94;
+res=0;
+calcularDolar(){
+  this.res = parseFloat(this.real)*Number(this.dolar);
+}
 
+AC=0;
+AGH=0;
+AT=0;
+media=0;
+calcularMedia(){
+  this.media = Number(this.AC)* 0.2+Number(this.AGH)*0.1+Number(this.AT)*0.7;
+}
 }
